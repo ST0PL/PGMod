6ce99032e39fdc8a890a5e863c4661bff7d750f6
st0pl <st0pl0gin@protonmail.com>
2026-02-11 01:12:44 +0700

    Add resource unpacker for imgui dlls

---
+++ PGMod/ResourceUnpacker.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace PGMod
+{
+    internal static class ResourceUnpacker
+    {
+
+        static readonly string[] dlls = ["cimgui.dll", "ImGuiImpl.dll"];
+        static readonly Assembly assembly = Assembly.GetExecutingAssembly();
+        static readonly string pePath = Directory.GetCurrentDirectory();
+
+        public static void PrepareResources()
+        {
+            foreach (var item in dlls)
+                if (!File.Exists(Path.Combine(pePath, item)))
+                    UnpackAsync(item);
+        }
+
+        private static void UnpackAsync(string resourceName)
+        {
+            using var resourceStream = assembly.GetManifestResourceStream("PGMod.Requirements." + resourceName);
+            using var fileStream = new FileStream(Path.Combine(pePath, resourceName), FileMode.Create);
+            resourceStream!.CopyTo(fileStream);
+        }
+    }
+}
